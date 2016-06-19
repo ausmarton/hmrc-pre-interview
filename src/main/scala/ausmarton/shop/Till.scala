85cf960978edc13db2c8e619faa16c0bd35514ed
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2016-06-19 19:52:48 +0100

    [Ausmarton] - added offers (apples: 2 for the price of 1 & oranges 3 for the price of 2)

--- src/main/scala/ausmarton/shop/Till.scala
+++ src/main/scala/ausmarton/shop/Till.scala
@@ -1,7 +1,14 @@
 package ausmarton.shop
 
-import ausmarton.shop.inventory.Fruit
+import ausmarton.shop.inventory.{Apple, Fruit, Orange}
 
 object Till {
-  def checkout(cart: Seq[Fruit]) = cart.map(_.cost).sum
+  def checkout(cart: Seq[Fruit]) = {
+    val (apples, oranges) = cart.partition(_ == Apple)
+    apples.grouped(2).size * Apple.cost +
+    oranges.grouped(3).map(_.size match {
+      case 2|3 => Orange.cost * 2
+      case n => Orange.cost * n
+    }).sum
+  }
 }
--- src/test/scala/ausmarton/shop/TillSpec.scala
+++ src/test/scala/ausmarton/shop/TillSpec.scala
@@ -4,6 +4,7 @@ import ausmarton.shop.inventory.{Apple, Orange}
 import org.scalatest.{Matchers, WordSpecLike}
 
 class TillSpec extends WordSpecLike with Matchers {
+  //Step 1: Shopping cart
   "empty cart" should {
     "cost 0" in {
       Till.checkout(Nil) shouldBe 0
@@ -16,27 +17,47 @@ class TillSpec extends WordSpecLike with Matchers {
     }
   }
 
-  "2 apples" should {
-    "cost 120p (£1.2)" in {
-      Till.checkout(Seq(Apple, Apple)) shouldBe 120
-    }
-  }
-
   "1 orange" should {
     "cost 25p" in {
       Till.checkout(Seq(Orange)) shouldBe 25
     }
   }
 
-  "1 apple and 1 orange" should {
-    "cost 85p" in {
-      Till.checkout(Seq(Apple, Orange)) shouldBe 85
+  "1 apple and 2 oranges" should {
+    "cost 110p (£1.1)" in {
+      Till.checkout(Seq(Apple, Orange, Orange)) shouldBe 110
+    }
+  }
+
+  //Step 2: Simple offers
+  "2 apples" should {
+    "cost 60p" in {
+      Till.checkout(Seq(Apple, Apple)) shouldBe 60
+    }
+  }
+
+  "2 oranges" should {
+    "cost 50p" in {
+      Till.checkout(Seq(Orange, Orange)) shouldBe 50
+    }
+  }
+
+  "3 oranges" should {
+    "cost 50p" in {
+      Till.checkout(Seq(Orange, Orange, Orange)) shouldBe 50
     }
   }
 
   "3 apples and 2 oranges" should {
-    "cost 230p (£2.3)" in {
-      Till.checkout(Seq(Apple, Orange, Orange, Apple, Apple)) shouldBe 230
+    "cost 170p (£1.7)" in {
+      Till.checkout(Seq(Apple, Orange, Orange, Apple, Apple)) shouldBe 170
+    }
+  }
+
+  "4 apples and 5 oranges" should {
+    "cost 220p (£2.2)" in {
+      Till.checkout(Seq(Apple, Apple, Apple, Apple,
+        Orange, Orange, Orange, Orange, Orange)) shouldBe 220
     }
   }
 }
