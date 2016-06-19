eff002494e816aacdf34785a1d97444516e95a06
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2016-06-19 19:55:16 +0100

    [Ausmarton] - refactored checkout to use min instead of a match case

--- src/main/scala/ausmarton/shop/Till.scala
+++ src/main/scala/ausmarton/shop/Till.scala
@@ -5,10 +5,8 @@ import ausmarton.shop.inventory.{Apple, Fruit, Orange}
 object Till {
   def checkout(cart: Seq[Fruit]) = {
     val (apples, oranges) = cart.partition(_ == Apple)
+
     apples.grouped(2).size * Apple.cost +
-    oranges.grouped(3).map(_.size match {
-      case 2|3 => Orange.cost * 2
-      case n => Orange.cost * n
-    }).sum
+    oranges.grouped(3).map(_.size.min(2)).sum * Orange.cost
   }
 }
